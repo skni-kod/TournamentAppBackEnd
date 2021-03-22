ec404794aaac673f263e29982fa5a174c05d845e
Brajan Miśkowicz <miskowicz@interia.pl>
2021-03-22 17:08:49 +0100

    Informacje o turnieju

--- Api/views.py
+++ Api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView
 from Api.serializers import *
 from Api.models import Player
+from Api.models import Tournament
 from rest_framework.response import Response
 
 
@@ -55,3 +56,55 @@ class PlayerViewSetDetail(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TournamentViewSetList(APIView):
+    queryset = Tournament.objects.none()
+
+    def get(self, format=None):
+        queryset = Tournament.objects.all()
+        serializer = TournamentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TournamentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TournamentViewSetDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Tournament.objects.get(pk=pk)
+        except Tournament.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk=None, format=None):
+        queryset=self.get_object(pk)
+        serializer = TournamentSerializer(queryset)
+        return Response(serializer.data)
+
+    def put(self, request, pk=None, format=None):
+        queryset = self.get_object(pk)
+        serializer = TournamentSerializer(queryset)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None, format=None):
+        queryset = self.get_object(pk)
+        queryset.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk=None, format=None):
+        queryset = self.get_object(pk)
+        serializer = TournamentSerializer(queryset, data=request.data, prtaial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
