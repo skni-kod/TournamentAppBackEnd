6957bb7cb935690594b103084ecd23d90620434c
SzymiDjud <58051054+SzymiDjud@users.noreply.github.com>
2021-05-19 19:17:59 +0200

    Token Lifetime

--- ApkaTurniejowaBackEnd/settings.py
+++ ApkaTurniejowaBackEnd/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from key import *
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -138,4 +139,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
