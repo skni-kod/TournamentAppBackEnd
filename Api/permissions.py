3239fa5af0a937497f08ff68b2aff6d66baa73d5
xRei <miskowicz@interia.pl>
2021-06-08 01:33:23 +0200

    Poprawki w permisjach

--- Api/permissions.py
+++ Api/permissions.py
@@ -1,6 +1,5 @@
 from rest_framework import permissions
-from django.contrib.auth.models import AnonymousUser, User
-from Api.models import CustomUser
+from django.contrib.auth.models import AnonymousUser
 
 
 class IsReadOnly(permissions.BasePermission):
@@ -21,20 +20,11 @@ class IsNotAuthorised(permissions.BasePermission):
         return bool(type(request.user) is AnonymousUser)
 
 
-class IsAuthorisedAndOwner(permissions.BasePermission):
-    # 'You must be the owner of this object.'
-    def has_permission(self, request, view):
-        return bool(type(request.user) is not AnonymousUser)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.user.email or request.user.is_superuser == True)
-
-
 class IsPlayer(permissions.BasePermission):    
     # 'You have to be Player'
     def has_permission(self, request, view):
         return bool( ( (type(request.user) is not AnonymousUser)  and request.user.groups.filter(name='Players').exists() ) or request.user.is_superuser == True)
         
-
 
 class IsJudge(permissions.BasePermission):    
     # 'You have to be Judge'
@@ -46,59 +36,3 @@ class IsAdmin(permissions.BasePermission):
     # 'You have to be admin - to have superuser extrafield set to True'
     def has_permission(self, request, view):
         return bool( (type(request.user) is not AnonymousUser)  and request.user.is_superuser == True)
-
-        
-class IsTournamentJudge(permissions.BasePermission):    
-    # 'You have to be tournament's Judge'
-    def has_permission(self, request, view):
-        return bool( ((type(request.user) is not AnonymousUser)  and request.user.groups.filter(name='Judges').exists() ) or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.judge.user.email)
-
-
-class IsGameJudge(permissions.BasePermission):    
-    # 'You have to be Judge of game's tournament'
-    def has_permission(self, request, view):
-        return bool( ((type(request.user) is not AnonymousUser)  and request.user.groups.filter(name='Judges').exists() ) or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.tournament.judge.user.email)
-
- 
-class IsResultJudge(permissions.BasePermission):    
-    # 'You have to be Judge of result's tournament'
-    def has_permission(self, request, view):
-        return bool( ((type(request.user) is not AnonymousUser)  and request.user.groups.filter(name='Judges').exists() ) or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.tournament.judge.user.email or request.method in permissions.SAFE_METHODS)             
-
-
-class IsNotificationOwner(permissions.BasePermission):    
-    # 'You have to be Judge of game's tournament'
-    def has_permission(self, request, view):
-        return bool( ((type(request.user) is not AnonymousUser)  and request.user.groups.filter(name='Players').exists() ) or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.notification.player.user.email)
-
-
-class IsUserOwner(permissions.BasePermission):    
-    # 'You have to be Judge of game's tournament'
-    def has_permission(self, request, view):
-        return bool( (type(request.user) is not AnonymousUser)   or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.email)
-
-
-class IsProfileOwner(permissions.BasePermission):    
-    # 'You have to be Profile owner'
-    def has_permission(self, request, view):
-        return bool( (type(request.user) is not AnonymousUser)   or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return (request.user.email == obj.user.email)
-
-
-class IsJudgeOwner(permissions.BasePermission):    
-    # 'You have to be Judge profile owner'
-    def has_permission(self, request, view):
-        return bool( (type(request.user) is not AnonymousUser)   or request.user.is_superuser == True)
-    def has_object_permission(self, request, view, obj):
-        return bool(request.user.email == obj.user.email)
